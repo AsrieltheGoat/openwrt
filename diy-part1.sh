fadd636bf5cba2344516aaa1fab6ec2ea6d9c70d
Asriel Nguyen <asriel@dreemurrs.eu.org>
2024-07-25 20:13:32 +0700

    change: add repos

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,8 +11,17 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '/^src-git other https:\/\/github.com\/Lienol\/openwrt-package.git;other$/d' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-link custom /home/runner/openwrt/packages' >>feeds.conf.default
+
+# sms-tool
+echo 'src-git smstool https://github.com/4IceG/luci-app-sms-tool' >>feeds.conf.default
+echo 'src-git 3ginfo https://github.com/4IceG/luci-app-3ginfo-lite' >>feeds.conf.default
+
+# modemnanager luci
+echo 'src-git luci_proto_modemmanager https://github.com/nickberry17/luci-proto-modemmanager' >>feeds.conf.default
